fea151cfa60feb1836755d0ff1fd9ecc67c4953d
sarapatriciaps <sara.patriciaps@gmail.com>
2023-10-07 15:08:07 -0300

    formatacao test_1 e test_3

--- tests/test_3.py
+++ tests/test_3.py
@@ -1,6 +1,8 @@
 import time
 import pytest
+
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from tests.conftest import URL
 from tests.conftest import url_pag_prod
