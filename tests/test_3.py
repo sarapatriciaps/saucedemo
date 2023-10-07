f1ff602c9ba3c4bec14245cb03fcba043b4fd534
sarapatriciaps <sara.patriciaps@gmail.com>
2023-10-07 14:28:08 -0300

    formatacao dos test_2 e test_3

--- tests/test_3.py
+++ tests/test_3.py
@@ -5,27 +5,28 @@ from selenium.webdriver.common.by import By
 from tests.conftest import URL
 from tests.conftest import url_pag_prod
 
+
 class Teste3:
 
     def test_click_login_btn(self, setup):
 
-        #instancia do obj driver (conftest)
+        # instancia do obj driver (conftest)
         driver = setup
 
-        #buscar botao login
-        login_btn = driver.find_element(By.ID, 'login-button') #busca elemento por Id
-        #driver.find_element(By.CSS_SELECTOR, '#login-button') #busca elemento por CSS Selector
-        #driver.find_element(By.XPATH,'//*[@id="login-button"]') #busca elemento por Xpath dinamico
-        #driver.find_element(By.XPATH,'/html/body/div/div/div[2]/div[1]/div/div/form/input') #busca elemento por Xpath full
+        # buscar botao login
+        login_btn = driver.find_element(By.ID, 'login-button')  # busca elemento por Id
+        # driver.find_element(By.CSS_SELECTOR, '#login-button') #busca elemento por CSS Selector
+        # driver.find_element(By.XPATH,'//*[@id="login-button"]') #busca elemento por Xpath dinamico
+        # driver.find_element(By.XPATH,'/html/body/div/div/div[2]/div[1]/div/div/form/input') #busca elemento por Xpath full
 
-        #clicar no botao login
+        # clicar no botao login
         login_btn.click()
         time.sleep(2)
 
-        #validar que mudou de pagina apos click
-        assert driver.current_url == URL,'Aplicacao nao permaneceu na mesma pagina!'
+        # validar que mudou de pagina apos click
+        assert driver.current_url == URL, 'Aplicacao nao permaneceu na mesma pagina!'
 
-        #validar que msg de erro foi exibida
+        # validar que msg de erro foi exibida
         msg_erro = driver.find_element(By.CLASS_NAME, 'error-message-container').text
         assert msg_erro == 'Epic sadface: Username is required', 'Erro ao validar mensagem de erro!'
 
@@ -34,19 +35,19 @@ class Teste3:
         # instancia do obj driver (conftest)
         driver = setup
 
-        #inserir usuario
-        driver.find_element(By.ID,'user-name').send_keys('standard_user')
-        #inserir senha
-        driver.find_element(By.CSS_SELECTOR,'[data-test="password"]').send_keys('secret_sauce')
-        #click btn login
+        # inserir usuario
+        driver.find_element(By.ID, 'user-name').send_keys('standard_user')
+        # inserir senha
+        driver.find_element(By.CSS_SELECTOR, '[data-test="password"]').send_keys('secret_sauce')
+        # click btn login
         driver.find_element(By.ID, 'login-button').click()
 
         time.sleep(2)
 
-        #validar se pagina de produtos foi apresentada (por URL)
+        # validar se pagina de produtos foi apresentada (por URL)
         assert driver.current_url == url_pag_prod, 'URL invalida! Pagina de produtos nao acessada.'
 
-        #validar se pagina de produtos foi apresentada (por objeto "titulo")
+        # validar se pagina de produtos foi apresentada (por objeto "titulo")
         tittle_text = driver.find_element(By.CLASS_NAME, 'title').text
         assert tittle_text == 'Products', 'TITULO da pagina de produtos NAO APRESENTADO!'
 
